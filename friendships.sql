1324711e97a68e3fac7d558be0ee49709e1bd16b
Brett Jory <bjory7@gmail.com>
2019-03-27 11:52:12 -0700

    queried all friendships, display first and last names of both friends. final problem finished, assignment complete.

--- friendships.sql
+++ friendships.sql
@@ -47,3 +47,7 @@ delete from friendships
 where user_id = 2 and friend_id = 5;
 
 -- 7. Return all friendships, displaying just the first and last name of both friends
+select users.first_name, users.last_name, users2.first_name as friend_first_name, users2.last_name as friend_last_name
+from users
+left join friendships on users.id = friendships.user_id
+inner join users as users2 on friendships.friend_id = users2.id;
