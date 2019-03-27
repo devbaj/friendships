1d98ac3e9ab5fe911bb9a700ad92aa76835ce404
Brett Jory <bjory7@gmail.com>
2019-03-27 08:52:15 -0700

    queried count of all friendships

--- friendships.sql
+++ friendships.sql
@@ -5,9 +5,12 @@ from users
 left join friendships on users.id = friendships.user_id
 inner join users as users2 on users2.id = friendships.friend_id; -- check w instructor to see; left join added null results obviously, inner join seems more appropriate
 
--- return all users who are friends with Kermit, display their names in results
+-- 1. Return all users who are friends with Kermit, display their names in results
 select users.first_name, users.last_name, users2.first_name as friend_first_name, users2.last_name as friend_last_name
 from users
 left join friendships on users.id = friendships.friend_id and users.first_name = "Kermit"
 inner join users as users2 on friendships.user_id = users2.id;
 
+-- 2. Return the count of all friendships
+select count(friendships.id) as "Number of Friendships"
+from friendships;
